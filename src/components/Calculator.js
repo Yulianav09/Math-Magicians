dfbc6170ddaaf18e59b2077e3464eb002064f1c7
Yuliana Velez <velezrodriguez2001@gmail.com>
2023-08-09 22:05:25 -0500

    Added event handler

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,13 +1,41 @@
+import { useState, useEffect } from 'react';
 import Button from './Button';
 import './calculator.css';
+import calculate from '../logic/calculate';
 
 const buttons = ['AC', '+/-', '%', '÷', '7', '8', '9', 'x', '4', '5', '6', '-', '1', '2', '3', '+', '0', '.', '='];
-const buttonItem = buttons.map((label) => <Button key={label} labelTag={label} />);
-const Calculator = () => (
-  <div className="calculator">
-    <div className="result">0</div>
-    {buttonItem}
-  </div>
-);
+
+const Calculator = () => {
+  const [calculatorObject, setCalculatorObject] = useState({
+    total: null,
+    next: '0',
+    operation: null,
+  });
+
+  const onClickHandler = (buttonName) => {
+    let result = calculate(calculatorObject, buttonName);
+    if (result.next === null && result.total === null) result = { ...result, next: '0' };
+    setCalculatorObject(result);
+  };
+
+  useEffect(() => {}, [calculatorObject]);
+
+  const buttonItem = buttons.map((label) => (
+    <Button click={onClickHandler} key={label} labelTag={label} />
+  ));
+
+  return (
+    <div className="calculator">
+      <div className="result">
+        {calculatorObject.total}
+        {' '}
+        {calculatorObject.operation}
+        {' '}
+        {calculatorObject.next}
+      </div>
+      {buttonItem}
+    </div>
+  );
+};
 
 export default Calculator;
